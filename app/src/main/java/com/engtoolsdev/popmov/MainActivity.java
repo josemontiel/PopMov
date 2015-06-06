5bb241741a3fe1fd4bd2d575681bc619a9d81a52
Jose <engtoolsdev@gmail.com>
2015-06-06 08:53:29 -0430

    Repo created

--- app/src/main/java/com/engtoolsdev/popmov/MainActivity.java
+++ app/src/main/java/com/engtoolsdev/popmov/MainActivity.java
@@ -2,41 +2,37 @@ package com.engtoolsdev.popmov;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SwitchCompat;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
-import com.engtoolsdev.popmov.models.Movie;
 
 public class MainActivity extends AppCompatActivity {
-
-    Toolbar toolbar;
-    MainActivityFragment movieFragment;
-    TextView titleView;
-    SwitchCompat faveSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
-
-        titleView = (TextView) toolbar.findViewById(R.id.main_toolbar_title);
-        faveSwitch = (SwitchCompat) findViewById(R.id.fave_switch);
-
-        movieFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
-
-        setSupportActionBar(toolbar);
     }
 
-    public void loadMovie(Movie movie){
-        if(movieFragment != null){
-            movieFragment.loadMovie(movie);
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-    }
 
+        return super.onOptionsItemSelected(item);
+    }
 }
